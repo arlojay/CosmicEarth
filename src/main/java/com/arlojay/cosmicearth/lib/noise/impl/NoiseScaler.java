d74693c4f54130b08c7db62b873b01cd8e83aed6
Arlo Virta <35749102+arlojay@users.noreply.github.com>
2024-08-25 14:48:14 -0500

    Add individual scaling to NoiseScaler

--- src/main/java/com/arlojay/cosmicearth/lib/noise/impl/NoiseScaler.java
+++ src/main/java/com/arlojay/cosmicearth/lib/noise/impl/NoiseScaler.java
@@ -7,7 +7,10 @@ import org.hjson.JsonObject;
 
 public class NoiseScaler implements NoiseNode {
     private final NoiseNode noise;
-    private final double scalar;
+    private final double scaleX;
+    private final double scaleY;
+    private final double scaleZ;
+    private final double scaleW;
 
     public static void register() {
         NoiseLoader.registerNoiseNode("scale", (JsonObject options) -> {
@@ -16,34 +19,44 @@ public class NoiseScaler implements NoiseNode {
 
             JsonObject sourceNode = sourceObject.asObject();
             double scalar = options.getDouble("scalar", 1d);
+            double scaleX = options.getDouble("scale_x", scalar);
+            double scaleY = options.getDouble("scale_y", scalar);
+            double scaleZ = options.getDouble("scale_z", scalar);
+            double scaleW = options.getDouble("scale_w", scalar);
 
-            return new NoiseScaler(NoiseLoader.createNoiseNode(sourceNode), scalar);
+            return new NoiseScaler(NoiseLoader.createNoiseNode(sourceNode), scaleX, scaleY, scaleZ, scaleW);
         });
     }
 
     public NoiseScaler(NoiseNode noise, double scalar) {
+        this(noise, scalar, scalar, scalar, scalar);
+    }
+    public NoiseScaler(NoiseNode noise, double scaleX, double scaleY, double scaleZ, double scaleW) {
         this.noise = noise;
-        this.scalar = scalar;
+        this.scaleX = scaleX;
+        this.scaleY = scaleY;
+        this.scaleZ = scaleZ;
+        this.scaleW = scaleW;
     }
 
     @Override
     public double sample(double t) {
-        return noise.sample(t * scalar);
+        return noise.sample(t * scaleX);
     }
 
     @Override
     public double sample(double x, double y) {
-        return noise.sample(x * scalar, y * scalar);
+        return noise.sample(x * scaleX, y * scaleY);
     }
 
     @Override
     public double sample(double x, double y, double z) {
-        return noise.sample(x * scalar, y * scalar, z * scalar);
+        return noise.sample(x * scaleX, y * scaleY, z * scaleZ);
     }
 
     @Override
     public double sample(double x, double y, double z, double w) {
-        return noise.sample(x * scalar, y * scalar, z * scalar, w * scalar);
+        return noise.sample(x * scaleX, y * scaleY, z * scaleZ, w * scaleW);
     }
 
     @Override
@@ -54,7 +67,10 @@ public class NoiseScaler implements NoiseNode {
     @Override
     public String buildString() {
         return "@NoiseScaler" + NoiseDebugString.createPropertyList(
-                "scalar", scalar
+                "scaleX", scaleX,
+                "scaleY", scaleY,
+                "scaleZ", scaleZ,
+                "scaleW", scaleW
         ) + NoiseDebugString.buildStringSubnode(noise);
     }
 }
