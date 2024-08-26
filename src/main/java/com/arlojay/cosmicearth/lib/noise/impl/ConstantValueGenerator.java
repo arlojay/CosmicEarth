d207d568db8296d6655afb10265abc72aec1b6da
Arlo Virta <35749102+arlojay@users.noreply.github.com>
2024-08-26 12:28:59 -0500

    Add basic noisegraph value generators

---
+++ src/main/java/com/arlojay/cosmicearth/lib/noise/impl/ConstantValueGenerator.java
@@ -0,0 +1,55 @@
+package com.arlojay.cosmicearth.lib.noise.impl;
+
+import com.arlojay.cosmicearth.lib.noise.NoiseDebugString;
+import com.arlojay.cosmicearth.lib.noise.NoiseNode;
+import com.arlojay.cosmicearth.lib.noise.loader.NoiseLoader;
+import org.hjson.JsonObject;
+
+public class ConstantValueGenerator implements NoiseNode {
+    private final double value;
+
+    public static void register() {
+        NoiseLoader.registerNoiseNode("const",
+                (JsonObject options) -> new ConstantValueGenerator(
+                        NoiseLoader.getProps().getDouble("value", 0d)
+                )
+        );
+    }
+
+    public ConstantValueGenerator(double value) {
+        this.value = value;
+    }
+
+
+    @Override
+    public double sample(double t) {
+        return value;
+    }
+
+    @Override
+    public double sample(double x, double y) {
+        return value;
+    }
+
+    @Override
+    public double sample(double x, double y, double z) {
+        return value;
+    }
+
+    @Override
+    public double sample(double x, double y, double z, double w) {
+        return value;
+    }
+
+    @Override
+    public void setSeed(long seed) {
+
+    }
+
+    @Override
+    public String buildString() {
+        return "@Constant" + NoiseDebugString.createPropertyList(
+                "value", value
+        );
+    }
+}
---
+++ src/main/java/com/arlojay/cosmicearth/lib/noise/impl/CoordinateGenerator.java
@@ -0,0 +1,63 @@
+package com.arlojay.cosmicearth.lib.noise.impl;
+
+import com.arlojay.cosmicearth.lib.noise.NoiseDebugString;
+import com.arlojay.cosmicearth.lib.noise.NoiseGenerator;
+import com.arlojay.cosmicearth.lib.noise.loader.NoiseLoader;
+import org.hjson.JsonObject;
+
+public class CoordinateGenerator extends NoiseGenerator {
+    private final boolean doX;
+    private final boolean doY;
+    private final boolean doZ;
+    private final boolean doW;
+
+    public static void register() {
+        NoiseLoader.registerNoiseNode("coordinate", (JsonObject options) -> {
+            boolean doX = options.getBoolean("x", false);
+            boolean doY = options.getBoolean("y", false);
+            boolean doZ = options.getBoolean("z", false);
+            boolean doW = options.getBoolean("w", false);
+
+            return new CoordinateGenerator(doX, doY, doZ, doW);
+        });
+    }
+
+    public CoordinateGenerator(boolean doX, boolean doY, boolean doZ, boolean doW) {
+        super(0L, 0L);
+
+        this.doX = doX;
+        this.doY = doY;
+        this.doZ = doZ;
+        this.doW = doW;
+    }
+
+    @Override
+    public double sample(double t) {
+        return doX ? t : 0;
+    }
+
+    @Override
+    public double sample(double x, double y) {
+        return (doX ? x : 0) + (doY ? y : 0);
+    }
+
+    @Override
+    public double sample(double x, double y, double z) {
+        return (doX ? x : 0) + (doY ? y : 0) + (doZ ? z : 0);
+    }
+
+    @Override
+    public double sample(double x, double y, double z, double w) {
+        return (doX ? x : 0) + (doY ? y : 0) + (doZ ? z : 0) + (doW ? w : 0);
+    }
+
+    @Override
+    public String buildString() {
+        return "@CoordinateGenerator" + NoiseDebugString.createPropertyList(
+                "doX", doX,
+                "doY", doY,
+                "doZ", doZ,
+                "doW", doW
+        );
+    }
+}
