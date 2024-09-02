589a91c7c853b51bddd5af11e4a615ca3ca930a6
Arlo Virta <35749102+arlojay@users.noreply.github.com>
2024-09-02 15:51:41 -0500

    Give all structure types their own noise

--- src/main/java/com/arlojay/cosmicearth/worldgen/structure/WorldgenStructure.java
+++ src/main/java/com/arlojay/cosmicearth/worldgen/structure/WorldgenStructure.java
@@ -2,11 +2,41 @@ package com.arlojay.cosmicearth.worldgen.structure;
 
 import finalforeach.cosmicreach.blocks.BlockState;
 import finalforeach.cosmicreach.world.Zone;
+import finalforeach.cosmicreach.worldgen.noise.WhiteNoise;
+
+import java.util.Objects;
+import java.util.Random;
 
 public abstract class WorldgenStructure {
+    private final WhiteNoise randomNoise = new WhiteNoise();
+    private final Random random = new Random();
+
+    private float offsetX;
+    private float offsetY;
+    private float offsetZ;
+
+    public WorldgenStructure() {
+        offsetX = Float.intBitsToFloat(Objects.hash(this.getId()));
+        offsetY = Float.intBitsToFloat(Objects.hash(this.getId(), offsetX));
+        offsetZ = Float.intBitsToFloat(Objects.hash(this.getId(), offsetY));
+
+        offsetX %= 1000f;
+        offsetY %= 1000f;
+        offsetZ %= 1000f;
+    }
+
+    protected abstract String getId();
+
     protected static BlockState getBlockStateInstance(String blockStateId) {
         return BlockState.getInstance(blockStateId);
     }
 
-    public abstract void generate(long seed, Zone zone, int globalX, int globalY, int globalZ);
+    public final void generate(long seed, Zone zone, int globalX, int globalY, int globalZ) {
+        randomNoise.setSeed(seed);
+        var randseed = (double) randomNoise.noise3D(globalX + offsetX, globalY + offsetY, globalZ + offsetZ);
+        random.setSeed(Double.doubleToLongBits(randseed));
+
+        generate(random, zone, globalX, globalY, globalZ);
+    }
+    protected abstract void generate(Random random, Zone zone, int globalX, int globalY, int globalZ);
 }
